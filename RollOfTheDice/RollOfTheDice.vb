2802b9f49106a234367493be77e565e957beabc1
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-02-15 09:01:10 -0700

    Start of Roll Of The Dice Project

---
+++ RollOfTheDice/RollOfTheDice.vb
@@ -0,0 +1,21 @@
+﻿Option Explicit On
+Option Strict On
+
+'Aftanom Anfilofieff
+'RCET0265
+'Spring 2021
+'Roll Of The Dice
+'https://github.com/AftaAnfi/Roll-Of-The-Dice.git
+Module RollOfTheDice
+
+
+
+    Sub Main()
+
+
+
+    End Sub
+
+
+
+End Module
